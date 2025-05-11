a51e096c95b8526a7cd5c6369dc2ff79be6f6a09
Manu <manuelass.dev@gmail.com>
2025-05-11 17:51:02 -0300

    criando video background

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // Menu Mobile
-
 const btnMobile = document.getElementById('btn-mobile');
 const menuMobile = document.getElementById('menu-mobile');
 
@@ -12,3 +11,20 @@ menuMobile.addEventListener('click', () => {
     menuMobile.classList.add('hidden')
     menuMobile.classList.remove('flex')
 })
+
+// Video
+const video = document.getElementById('video');
+const temperatura = document.getElementById('temperatura');
+const tempo = document.getElementById('tempo');
+const dia = document.getElementById('dia');
+
+const random = Math.floor(Math.random() * 10) + 20;
+temperatura.innerText = `${random}°`;
+tempo.innerText = random < 25 ? `🌧️ ${random + 5}%` : '⛅';
+
+const date = new Date();
+dia.innerText = date.toLocaleDateString('pt-BR', {
+    weekday: 'long'
+}).replace('-feira', '');
+
+video.src = random < 25 ? "./img/video_chuva.mp4" : "./img/video_sol.mp4"
